a50d2b38480c53234eaab9629e923522af61ee20
ShadowChris <54981552+ShadowChris@users.noreply.github.com>
2024-01-17 11:25:58 +0800

    hw1

--- Homework1/Assignment1/main.cpp
+++ Homework1/Assignment1/main.cpp
@@ -3,6 +3,12 @@
 #include <eigen3/Eigen/Eigen>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+using namespace std;
+/**
+ * @brief 
+ * 作业参考链接：https://scarletsky.github.io/2020/06/09/games101-notes-transformation/
+ * 提高题+代码理解：https://blog.csdn.net/ycrsw/article/details/123834579
+ */
 
 constexpr double MY_PI = 3.1415926;
 
@@ -26,6 +32,13 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    // 实现图形绕z轴旋转的函数，即定义绕z轴的旋转矩阵。
+    float radian = rotation_angle / 180 * MY_PI;
+
+    model << cos(radian), -sin(radian), 0, 0,
+             sin(radian), cos(radian), 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 
     return model;
 }
@@ -40,8 +53,36 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    // 透视投影：Mp = Mo * Mp->o，Mo：正交投影矩阵；Mp->o：将透视图压缩成正交图
+    Eigen::Matrix4f Mpo;
+
+    Mpo << zNear, 0, 0, 0,
+           0, zNear, 0, 0,
+           0, 0, zNear + zFar, -zNear * zFar,
+           0, 0, 1, 0;
+    
+    Eigen::Matrix4f Mo = Eigen::Matrix4f::Identity();
+
+    // 构建Mo
+    float n = zNear, f = zFar;
+    float t = n * tan(eye_fov / 2 * MY_PI / 180.0), b = -t;
+    float r = aspect_ratio * t, l = -r;
+
+    Mo(0, 0) = 2 / (r - l);
+    Mo(1, 1) = 2 / (t - b);
+    Mo(2, 2) = 2 / (n - f);
+
+    Mo(0, 3) = -(r + l) / (r - l);
+    Mo(1, 3) = -(t + b) / (t - b);
+    Mo(2, 3) = -(n + f) / (n - f);
+
+    projection = Mo * Mpo;
 
     return projection;
+}
+
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle) {
+
 }
 
 int main(int argc, const char** argv)
